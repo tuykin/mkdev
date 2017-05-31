5533f425dec4e324665d9427c679a59f3b8a857e
Anvar Tuykin <tuykin@gmail.com>
2017-05-31 17:46:57 +0300

    Add searching by query string from ARGV

--- 2_movies.rb
+++ 2_movies.rb
@@ -1,10 +1,26 @@
+q = ARGV[0].downcase
+
+if q.nil?
+  puts 'Enter query to search movie'
+  return
+end
+
+found = false
+
 File.open('movies.txt', 'r') do |f|
   f.each_line do |line|
     movie_info = line.split('|')
+
     name = movie_info[1]
     rating = movie_info[7]
     stars_count = ((rating.to_f - 8) * 10).round
     stars = '*' * stars_count
-    puts "#{name}: #{stars}"
+
+    if !name.downcase.index(q).nil?
+      puts "#{name}: #{stars}"
+      found = true
+    end
   end
 end
+
+puts 'no movie found' unless found

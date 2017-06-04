60518e3a861cd492474558879000f607d39c549c
Anvar Tuykin <tuykin@gmail.com>
2017-06-04 03:06:24 +0300

    Implement MovieCollection methods

--- 5_oop/demo.rb
+++ 5_oop/demo.rb
@@ -1,5 +1,21 @@
 load 'movie.rb'
 load 'movie_collection.rb'
 
-collection = MovieCollection.new('../movies.txt')
-puts collection.all.first(5)#.map(&:inspect)
+movies = MovieCollection.new('../movies.txt')
+puts '*** Sorting ***'
+%i(link title year month date country genres duration rating producer actors).each do |field|
+  puts movies.sort_by(field).first(5)
+  puts
+end
+
+puts '*** Filtering ***'
+[{ genres: 'Comedy' }, { country: 'USA' }, { country: 'Russia' }].each do |facet|
+  puts movies.filter(facet).first(5)
+  puts
+end
+
+puts '*** Stats ***'
+%i(month year country producer actors genres).each do |field|
+  puts movies.stats(field).inspect
+  puts
+end
--- 5_oop/movie_collection.rb
+++ 5_oop/movie_collection.rb
@@ -10,7 +10,6 @@ class MovieCollection
   def initialize(file_name)
     raise FileNotFoundError unless File.file?(file_name)
     @movies = parse_file(file_name)
-    puts 'this is collection'
   end
 
   def all
@@ -18,18 +17,27 @@ class MovieCollection
   end
 
   def sort_by(field)
+    all.sort_by(&field)
   end
 
   def filter(facet)
+    key = facet.keys.first
+    value = facet.values.first
+
+    return unless %i(genres country).include?(key)
+
+    all.select { |m| m.send(key).include?(value) } || []
   end
 
   def stats(field)
+    return unless %i(month year country producer actors genres).include?(field)
+    all.map { |m| m.send(field) }.flatten.each_with_object(Hash.new(0)) { |o, h| h[o] += 1 }
   end
 
-  def to_s
-  end
-
-  def inspect
+  def print_stats(field)
+    stats(field).sort.each do |field, count|
+      puts "#{field}: #{count}"
+    end
   end
 
   private

f25e1a7c3dd694b6115690e275fc0b5245d20c2b
Anvar Tuykin <tuykin@gmail.com>
2017-06-07 01:41:24 +0300

    Add Movie.build to build descendants

--- 6_inheritance/lib/movie.rb
+++ 6_inheritance/lib/movie.rb
@@ -9,16 +9,23 @@ class Movie
 
   def initialize(collection = nil, params)
     @collection = collection
+    params.each { |k, v| send("#{k}=", v) }
+  end
 
-    params.to_h.merge({
-      year: params[:year].to_i,
-      duration: params[:duration].to_i,
-      rating: params[:rating].to_f,
-      genres: params[:genres].split(','),
-      actors: params[:actors].split(','),
-      month: get_month(params[:date]),
-      date: parse_date(params[:date])
-    }).each { |k, v| send("#{k}=", v) }
+  def self.build(collection = nil, params)
+    params = prepare_data(params)
+    case params[:year]
+    when 1900...1945
+      AncientMovie.new(collection, params)
+    when 1945...1968
+      ClassicMovie.new(collection, params)
+    when 1958...2000
+      ModernMovie.new(collection, params)
+    when 2000..Date.today.year
+      NewMovie.new(collection, params)
+    else
+      Movie.new(collection, params)
+    end
   end
 
   def has_genre?(genre)
@@ -34,14 +41,26 @@ class Movie
     "#{title} | #{country} | #{year} | #{producer} | #{genres} | #{actors} \n"
   end
 
+  def self.prepare_data(params)
+    params.to_h.merge({
+      year: params[:year].to_i,
+      duration: params[:duration].to_i,
+      rating: params[:rating].to_f,
+      genres: params[:genres].split(','),
+      actors: params[:actors].split(','),
+      month: get_month(params[:date]),
+      date: parse_date(params[:date])
+    })
+  end
+
   private
 
-  def parse_date(str)
+  def self.parse_date(str)
     (2 - str.count('-')).times { str << '-01' }
     Date.parse(str)
   end
 
-  def get_month(str)
+  def self.get_month(str)
     str.split('-')[1]&.to_i || 0
   end
 end
--- 6_inheritance/spec/movie_spec.rb
+++ 6_inheritance/spec/movie_spec.rb
@@ -5,6 +5,50 @@ require 'modern_movie'
 require 'new_movie'
 
 RSpec.describe Movie do
+  it '#initialize' do
+    movie = Movie.new(movie_params)
+    expect(movie.title).to eq(movie_params[:title])
+    expect(movie.year).to eq(movie_params[:year])
+    expect(movie.actors).to eq(movie_params[:actors])
+  end
+
+  describe '#build' do
+    it 'Movie' do
+      year = '1899'
+      movie = Movie.build(movie_params_raw.merge(year: year))
+      expect(movie.year).to eq(year.to_i)
+      expect(movie.class).to eq(Movie)
+    end
+
+    it 'AncientMovie' do
+      year = '1900'
+      movie = Movie.build(movie_params_raw.merge(year: year))
+      expect(movie.year).to eq(year.to_i)
+      expect(movie.class).to eq(AncientMovie)
+    end
+
+    it 'ClassicMovie' do
+      year = '1945'
+      movie = Movie.build(movie_params_raw.merge(year: year))
+      expect(movie.year).to eq(year.to_i)
+      expect(movie.class).to eq(ClassicMovie)
+    end
+
+    it 'ModernMovie' do
+      year = '1968'
+      movie = Movie.build(movie_params_raw.merge(year: year))
+      expect(movie.year).to eq(year.to_i)
+      expect(movie.class).to eq(ModernMovie)
+    end
+
+    it 'NewMovie' do
+      year = '2000'
+      movie = Movie.build(movie_params_raw.merge(year: year))
+      expect(movie.year).to eq(year.to_i)
+      expect(movie.class).to eq(NewMovie)
+    end
+  end
+
   describe '#to_s' do
     it 'AncientMovie' do
       movie = AncientMovie.new(movie_params)
@@ -28,6 +72,10 @@ RSpec.describe Movie do
   end
 
   def movie_params
+    Movie.prepare_data(movie_params_raw)
+  end
+
+  def movie_params_raw
     {
       link: "http://imdb.com/title/tt0111161/?ref_=chttp_tt_1",
       title: "The Shawshank Redemption",

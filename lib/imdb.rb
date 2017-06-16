da329975fe84e914fdb9e07c0dc765978d449b09
Anvar Tuykin <tuykin@gmail.com>
2017-06-16 11:43:41 +0300

    Add load path to module

--- lib/imdb.rb
+++ lib/imdb.rb
@@ -1,4 +1,7 @@
 module IMDB
+  libdir = File.dirname(__FILE__)
+  $LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
+
   require_relative 'imdb/movie'
   require_relative 'imdb/movie_collection'
   require_relative 'imdb/ancient_movie'
